30a7c04f299b3e5b4007558593d7287f9e1e2560
José Henrique <jhenriquevb11@gmail.com>
2024-02-07 23:34:39 -0300

    implementando detalhes do produto

--- produto/views.py
+++ produto/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
 from . import models
 
@@ -12,9 +13,11 @@ class ListaProdutos(ListView):
     context_object_name = "produtos"
     paginate_by = 10
 
-class DetalheProdutos(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse("Detalhe")
+class DetalheProdutos(DetailView):
+    model = models.Produto
+    template_name = "produto/detalhe.html"
+    context_object_name = "produto"
+    slug_url_kwarg = "slug"
 
 
 class AdicionarAoCarrinho(View):
